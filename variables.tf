9abeedda4709af26fed615895da5224caab5b68f
Peter Leung <pleung@hashicorp.com>
2020-06-26 00:15:02 -0400

    Update region

--- variables.tf
+++ variables.tf
@@ -1,35 +1,34 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default     = "us-east-1"
 }
-
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default     = "ami-2e1ef954"
 }
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.small"
+  default     = "t2.small"
 }
 
 variable "name" {
   description = "name to pass to Name tag"
-  default = "Provisioned by Terraform"
+  default     = "Provisioned by Terraform"
 }
 
 variable "owner" {
   description = "Name to pass to the Owner tag"
-  default = "Pizza-Pizza"
+  default     = "Pizza-Pizza"
 }
 
 variable "ttl" {
   description = "Hours until instances are reaped by N.E.P.T.R"
-  default = "3"
+  default     = "3"
 }
 
 variable "description" {
   description = "So meta"
-  default = "Foo"
+  default     = "Foo"
 }
